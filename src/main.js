00e94ef50c99513c85f5b8ce883c30ff4be8d9a4
george0710 <geojihdev@gmail.com>
2019-11-09 19:30:14 +0900

    main.jsにセミコロンを追加

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'va/lib/css'
-import 'va/lib/script'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'va/lib/css';
+import 'va/lib/script';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
